b8b3e96ba289c4db7901c22e53a1b9b9b9197080
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-05-09 13:43:12 +0300

    feat: extract features section from HomePage with typed data and icons

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,13 +1,11 @@
 import Hero from "@/components/layout/Hero";
 import FeaturesSection from "../components/FeaturesSection";
-import ProductsSection from "../components/ProductsSection";
 
 function HomePage() {
   return (
     <>
       <Hero />
       <FeaturesSection />
-      <ProductsSection />
     </>
   );
 }
